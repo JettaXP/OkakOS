469a66c3e03654f972da90852cb4cac42a94f2f4
JettaX <113067749+JettaXP@users.noreply.github.com>
2026-05-16 16:51:12 +0300

    Refactor fish prompt and command not found functions

--- airootfs/etc/fish/conf.d/okaksh.fish
+++ airootfs/etc/fish/conf.d/okaksh.fish
@@ -1,22 +1,24 @@
-function fish_prompt
-    set -l current_user (whoami)
-    set -l current_dir (prompt_pwd)
-    
-    set_color cyan
-    echo -n "$current_user "
-    
-    set_color normal
-    echo -n "@AppData\\Roaming\\Microsoft\\Windows\\Recent\\sixseven.lnk "
-    
-    set_color yellow
-    echo -n "$current_dir "
-    
-    set_color normal
-    echo -n "> "
-end
-
-function fish_command_not_found
-    set_color red
-    echo "Poshel nahuuy kozel 🤝" >&2
-    set_color normal
+fish_add_path ~/.local/bin
+if status is-interactive
+    set fish_greeting
+    function fish_prompt
+        set -l current_user (whoami)
+        set -l current_dir (prompt_pwd)
+        set -l current_host (hostname)
+        set_color cyan
+        echo -n "$current_user"
+        set_color normal
+        echo -n "@"
+        set_color green
+        echo -n "$current_host "
+        set_color yellow
+        echo -n "$current_dir "
+        set_color normal
+        echo -n "> "
+    end
+    function fish_command_not_found
+        set_color red
+        echo "Poshel nahuuy kozel 🤝" >&2
+        set_color normal
+    end
 end
