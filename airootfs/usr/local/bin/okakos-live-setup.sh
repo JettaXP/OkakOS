d6f81c2ba79cd3583e662232cfc8747a82a62d54
jettaxp <jettaxp@users.noreply.github.com>
2026-06-04 03:01:33 +0300

    fix root login on tty by appending fish to shells dynamically

--- airootfs/usr/local/bin/okakos-live-setup.sh
+++ airootfs/usr/local/bin/okakos-live-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 chmod +x /usr/local/bin/* 2>/dev/null || true
+grep -q "/usr/bin/fish" /etc/shells || echo "/usr/bin/fish" >> /etc/shells
 systemctl disable sddm.service -f 2>/dev/null || true
 rm -f /etc/systemd/system/display-manager.service 2>/dev/null || true
 exit 0
